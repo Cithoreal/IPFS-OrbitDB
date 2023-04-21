980630b368d404c0f935db62d5586ea87617119b
Chris <syrhicalacid@gmail.com>
2023-04-20 21:55:44 -0400

    working test

--- Scripts/IPFSOrbitDB.js
+++ Scripts/IPFSOrbitDB.js
@@ -1,6 +1,7 @@
 import * as IPFS_CORE from "ipfs-core";
 import orbitdb from "orbit-db";
 import fs from "fs";
+import { multiaddr } from 'multiaddr'
 //Create IPFS
 class IPFSOrbitDB {
 
@@ -42,10 +43,13 @@ class IPFSOrbitDB {
       kvStoreOptions
     );
     await this.thoughtDictDB.load();
+ 
+    console.log(this.thoughtDictDB.all);
     //console.log(await this.node.bootstrap.list())
     //console.log(await this.node.swarm.localAddrs())
     //console.log(await this.node.swarm.addrs());
    // console.log(await this.node.swarm.peers())
+
     this.user = await this.orbitdb.kvstore("user", this.defaultOptions);
     await this.user.load();
     await this.user.set("ThoughtDictionary", this.thoughtDictDB.id);
@@ -73,80 +77,83 @@ class IPFSOrbitDB {
 
     // when the OrbitDB docstore has loaded, intercept this method to
     // carry out further operations.
-   // this.onready();
+    this.onready();
+
+    await node.addToDB(['-3', "1", '2', '3']);
+     await node.getFromDB(['-a']);
   }
 
   async onready() {
-    await this.connectToPeer("12D3KooWGNNm7qG3TWk3ZmoDHf4UsKqaW7YbCJTMe664734iqzAN");
-    ondbdiscovered = (db) => console.log(db.all);
+    await this.connectToPeer('12D3KooWBKAmJaPp1kP6BAzieTzaEnfteUicUaC3eiaUckFpBvwP', '/p2p-circuit/ipfs/');
+    this.ondbdiscovered = (db) => console.log(db.all);
 
-    onpeeronline = console.log;
-    onpeernotfound = () => {
+    this.onpeeronline = console.log;
+    this.onpeernotfound = () => {
       throw e;
     };
 
-    queryCatalog();
+    this.queryCatalog();
   }
 
-  async addToDB() {
+  async addToDB(thoughts) {
     var nodes = [];
     //Check each value to see if it is a file or not
     //Add file conent ids to the value array
-    for (let i = 3; i < process.argv.length; i++) {
-      nodes.push(process.argv[i]);
-      if (fileExists(process.argv[i])) {
+    for (let i = 1; i < thoughts.length; i++) {
+      nodes.push(thoughts[i]);
+      if (this.fileExists(thoughts[i])) {
         console.log("adding file to ipfs");
         const file = await ipfs.add({
-          path: process.argv[i],
-          content: fs.createReadStream(process.argv[i]),
+          path: thoughts[i],
+          content: fs.createReadStream(thoughts[i]),
         });
         await ipfs.pin.add(file.cid);
         //console.log(file.cid.toString())
-        nodes.push(process.argv[i].substring(0, process.argv[i].indexOf(".")));
-        nodes.push(process.argv[i].substring(process.argv[i].indexOf(".")));
+        nodes.push(thoughts[i].substring(0, thoughts[i].indexOf(".")));
+        nodes.push(thoughts[i].substring(thoughts[i].indexOf(".")));
         nodes.push(file.cid.toString());
         //console.log(nodes)
       }
     }
     for (let i = 0; i < nodes.length; i++) {
-      if ((await db.get(nodes[i])) == null) {
-        await db.put(nodes[i], { values: [] });
+      if ((await this.thoughtDictDB.get(nodes[i])) == null) {
+        await this.thoughtDictDB.put(nodes[i], { values: [] });
       }
     }
-    if (process.argv[2] == "-1" || process.argv[2] == "-2") {
+    if (thoughts[0] == "-1" || thoughts[0] == "-2") {
       for (let i = 0; i < nodes.length; i++) {
         for (let j = i; j < nodes.length; j++) {
           if (i != j) {
-            var node_values = await db.get(nodes[i]).values;
+            var node_values = await this.thoughtDictDB.get(nodes[i]).values;
             if (!node_values.includes(nodes[j])) {
               node_values.push(nodes[j]);
             }
-            await db.put(nodes[i], { values: node_values });
+            await this.thoughtDictDB.put(nodes[i], { values: node_values });
           }
         }
       }
     }
-    if (process.argv[2] == "-2") {
+    if (thoughts[0] == "-2") {
       for (let i = nodes.length - 1; i >= 0; i--) {
         for (let j = i; j >= 0; j--) {
           if (i != j) {
-            var node_values = await db.get(nodes[i]).values;
+            var node_values = await this.thoughtDictDB.get(nodes[i]).values;
             if (!node_values.includes(nodes[j])) {
               node_values.push(nodes[j]);
             }
-            await db.put(nodes[i], { values: node_values });
+            await this.thoughtDictDB.put(nodes[i], { values: node_values });
           }
         }
       }
     }
-    if (process.argv[2] == "-3") {
+    if (thoughts[0] == "-3") {
       for (let i = 0; i < nodes.length; i++) {
         if (i != nodes.length - 1) {
-          var node_values = await db.get(nodes[i]).values;
+          var node_values = await this.thoughtDictDB.get(nodes[i]).values;
           if (!node_values.includes(nodes[i + 1])) {
             node_values.push(nodes[i + 1]);
           }
-          await db.put(nodes[i], { values: node_values });
+          await this.thoughtDictDB.put(nodes[i], { values: node_values });
         }
       }
     }
@@ -161,25 +168,26 @@ class IPFSOrbitDB {
     }
   }
 
-  async getFromDB() {
-    if (process.argv[2] == "-a" || process.argv[2] == "-all") {
-      console.log(db.all);
-      await db.close();
-      ipfs.stop();
-      return db.all;
+  async getFromDB(thoughts) {
+
+    if (thoughts[0] == "-a" || thoughts[0] == "-all") {
+      console.log(this.thoughtDictDB.all);
+      //await this.thoughtDictDB.close();
+      //ipfs.stop();
+      return this.thoughtDictDB.all;
     }
     var node_dictionary = {};
 
-    for (let i = 2; i < process.argv.length; i++) {
-      if ((await db.get(process.argv[i])) != null) {
-        node_dictionary[process.argv[i]] = await db.get(process.argv[i]).values;
+    for (let i = 1; i < thoughts.length; i++) {
+      if ((await this.thoughtDictDB.get(thoughts[i])) != null) {
+        node_dictionary[thoughts[i]] = await this.thoughtDictDB.get(thoughts[i]).values;
       } else {
-        node_dictionary[process.argv[i]] = [];
+        node_dictionary[thoughts[i]] = [];
       }
     }
     console.log(node_dictionary);
-    await db.close();
-    ipfs.stop();
+    //await this.thoughtDictDB.close();
+    //ipfs.stop();
     return node_dictionary;
   }
 
@@ -218,11 +226,12 @@ class IPFSOrbitDB {
     return peers;
   }
 
-  async connectToPeer(multiaddr, protocol = "/dnsaddr/bootstrap.libp2p.io/p2p/") {
+  async connectToPeer(address, protocol = "/dnsaddr/bootstrap.libp2p.io/p2p/") {
+    var addr = multiaddr(protocol + address);
     console.log("attempt connect to peer");
-    console.log(protocol + multiaddr);
+    console.log(addr);
     try {
-      await this.node.swarm.connect(protocol + multiaddr);
+      await this.node.swarm.connect(addr);
     } catch (e) {
       throw e;
     }
@@ -294,10 +303,10 @@ class IPFSOrbitDB {
   }
 
   async queryCatalog() {
-    const peerIndex = NPP.peers.all();
-    const dbAddrs = Object.keys(peerIndex).map((key) => peerIndex[key].pieces);
+    const peerIndex = this.peers.all;
+    const dbAddrs = Object.keys(peerIndex).map((key) => peerIndex[key].thoughtDictDB);
 
-    const allPieces = await Promise.all(
+    const allThoughts = await Promise.all(
       dbAddrs.map(async (addr) => {
         const db = await this.orbitdb.open(addr);
         await db.load();
@@ -306,10 +315,10 @@ class IPFSOrbitDB {
       })
     );
 
-    return allPieces.reduce((flatPieces, pieces) => {
-      pieces.forEach((p) => flatPieces.push(p));
+    return allThoughts.reduce((flatThoughts, thoughts) => {
+      thoughts.forEach((p) => flatThoughts.push(p));
       return flatPieces;
-    }, this.pieces.get(""));
+    }, this.thoughtDictDB.get(""));
   }
 }
 
